ab3de061aefcab3b1b7983a179f8718362a9a972
Martin Toman <whomenope@outlook.com>
2019-06-04 17:45:23 +0200

    Get all uploaded books from directory

--- app/src/Controller/Home.php
+++ app/src/Controller/Home.php
@@ -6,6 +6,8 @@ use Core\Template;
 
 class Home extends AbstractController
 {
+  const BOOKS_DIR = 'books';
+
   public function __construct()
   {
     parent::__construct(new Template());
@@ -17,9 +19,29 @@ class Home extends AbstractController
       __METHOD__,
       [
         'title' => APP_NAME.' - Home',
-        'header' => 'Welcome to '.APP_NAME
+        'header' => 'Welcome to '.APP_NAME,
+        'books' => $this->dirToArray(getenv('STORAGE_ROOT') . self::BOOKS_DIR)
       ]
     );
 
   }
+
+  private function dirToArray (string $dir) : array
+  {
+    $result = array();
+
+    $cdir = scandir($dir);
+    foreach ($cdir as $key => $value) {
+      if (!in_array($value,array(".",".."))) {
+        if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+          $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+        }
+        else {
+          $result[] = $value;
+        }
+      }
+    }
+    return $result;
+  }
+
 }
